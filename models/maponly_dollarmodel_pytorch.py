1e761a4cfdcfd779b2eee414ce0ed09fe57d0929
roman <83784750+Rn9303@users.noreply.github.com>
2023-08-15 20:18:07 -0400

    Update maponly_dollarmodel_pytorch.py

--- models/maponly_dollarmodel_pytorch.py
+++ models/maponly_dollarmodel_pytorch.py
@@ -6,7 +6,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
-from transformers import AutoProcessor, CLIPModel
 
 seed = 7499629
 
